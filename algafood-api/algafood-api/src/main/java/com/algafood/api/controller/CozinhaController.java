18caf34cd5105da9c6e3277bd789de6ffd72b0fb
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-10-16 19:28:34 -0300

    End point buscar por id e xml

--- algafood-api/algafood-api/src/main/java/com/algafood/api/controller/CozinhaController.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/api/controller/CozinhaController.java
@@ -3,16 +3,19 @@ package com.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algafood.api.model.CozinhasXmlWrapper;
 import com.algafood.domain.model.Cozinha;
 import com.algafood.domain.repository.CozinhaRepository;
 
 
 @RestController 
-@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas")
 public class CozinhaController {
 	
 	@Autowired
@@ -22,6 +25,16 @@ public class CozinhaController {
 	@GetMapping
 	public List<Cozinha> listar(){
 		return cozinhaRepository.listar();
-		
 	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public CozinhasXmlWrapper listarXml(){
+		return new CozinhasXmlWrapper(cozinhaRepository.listar());
+	}
+	
+	@GetMapping("/{cozinhaId}")
+	public Cozinha buscar(@PathVariable Long cozinhaId) { 
+		return cozinhaRepository.buscar(cozinhaId);
+	}
+	
 }
