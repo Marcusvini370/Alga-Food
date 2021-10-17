1cecee3615c862d8b39e6f9b8430aa8a8a891bcd
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-10-17 19:20:22 -0300

    Respostas http

--- algafood-api/algafood-api/src/main/java/com/algafood/api/controller/CozinhaController.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/api/controller/CozinhaController.java
@@ -3,10 +3,14 @@ package com.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algafood.api.model.CozinhasXmlWrapper;
@@ -32,9 +36,22 @@ public class CozinhaController {
 		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
 	
+	
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId) { 
-		return cozinhaRepository.buscar(cozinhaId);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) { 
+		// o responseEntity representa uma resposta http onde pode ter  uma instância de cozinha nele
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		//return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+		//return ResponseEntity.ok(cozinha);
+		
+		HttpHeaders  headers = new HttpHeaders();
+		headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
+		
+		return ResponseEntity
+				.status(HttpStatus.FOUND)
+				.headers(headers)
+				.build();
 	}
 	
 }
