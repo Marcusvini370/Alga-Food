951b2324d0568a65cb54cc42326e29e5968dcf64
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-10-19 18:23:05 -0300

    End point PUT e DELETE de cozinha

--- algafood-api/algafood-api/src/main/java/com/algafood/api/controller/CozinhaController.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/api/controller/CozinhaController.java
@@ -2,14 +2,18 @@ package com.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -59,4 +63,39 @@ public class CozinhaController {
 		
 	}
 	
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId,
+			@RequestBody Cozinha cozinha){
+		
+		Cozinha cozinhaAual = cozinhaRepository.buscar(cozinhaId);
+		//cozinhaAual.setNome(cozinha.getNome());
+		
+		if(cozinhaAual != null) {
+		BeanUtils.copyProperties(cozinha, cozinhaAual, "id"); // copia os valores de cozinha e coloca dentro da cozinhaAtual, está ignoradano a cópia do id
+		
+		cozinhaRepository.salvar(cozinhaAual);
+		
+				return ResponseEntity.ok(cozinhaAual);
+		}
+		return ResponseEntity.notFound().build();
+		
+	}
+	
+	
+	@DeleteMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId){
+		try {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		if(cozinha != null) {
+		cozinhaRepository.remover(cozinha);
+		return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.notFound().build();
+		}catch(DataIntegrityViolationException e) {
+			return  ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
+		
+	}
+		
 }
