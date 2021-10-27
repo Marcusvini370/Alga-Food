ae124b4c04d35efae7cb00c4e71a53de4cce2508
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-10-27 15:12:59 -0300

    Consulta com criteria

--- algafood-api/algafood-api/src/main/java/com/algafood/infracstruture/repository/RestauranteRepositoryImpl.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/infracstruture/repository/RestauranteRepositoryImpl.java
@@ -1,15 +1,20 @@
 package com.algafood.infracstruture.repository;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.algafood.domain.model.Restaurante;
 import com.algafood.domain.repository.RestauranteReposiotryQueries;
+
 
 @Repository
 public class RestauranteRepositoryImpl implements RestauranteReposiotryQueries { 
@@ -20,15 +25,34 @@ public class RestauranteRepositoryImpl implements RestauranteReposiotryQueries {
 	@Override
 	public List<Restaurante> find(String nome, 
 			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		
-		var jpql = "from Restaurante where nome like :nome "
-				+ "and taxaFrete between :taxaInicial and :taxaFinal";
 		
-		return manager.createQuery(jpql, Restaurante.class)
-				.setParameter("nome",  "%" + nome + "%")
-				.setParameter("taxaInicial", taxaFreteInicial)
-				.setParameter("taxaFinal", taxaFreteFinal)
-				.getResultList();
+		 var criteria = builder.createQuery(Restaurante.class);
+		 
+		 var  root = criteria.from(Restaurante.class); //from Restaurante
+		 
+		 var predicates = new ArrayList<>();
+		 
+		 if (StringUtils.hasText(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+		 }
+		
+		 if (taxaFreteInicial != null) {
+			 predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+		 }
+		 
+		 if (taxaFreteFinal != null) {
+			 predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+		 }
+		 
+		
+		 
+		 criteria.where(predicates.toArray(new Predicate[0]));
+		
+		var query = manager.createQuery(criteria);
+		return query.getResultList();
+			
 	}
 	
 }
