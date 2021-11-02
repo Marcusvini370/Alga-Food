0f04c78bbbc7a4b01bb6495c79881c6f624ae772
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-11-02 20:10:46 -0300

    Adicionando classe Pedido e ItemPedido + script flyway

---
+++ algafood-api/algafood-api/src/main/java/com/algafood/domain/model/ItemPedido.java
@@ -0,0 +1,40 @@
+package com.algafood.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ItemPedido {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+	
+	private BigDecimal quantidade;
+	private BigDecimal precoUnitario;
+	private BigDecimal precoTotal;
+	private String observacao;
+	
+	
+	@ManyToOne
+    @JoinColumn(nullable = false)
+    private Pedido pedido;
+	
+	@ManyToOne
+    @JoinColumn(nullable = false)
+    private Produto produto;
+	
+}
