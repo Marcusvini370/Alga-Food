ae64aa6d68ea871e0f54e891e0b73e204b05e990
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-01-03 15:24:44 -0300

    Callback com flyway nos testes

--- algafood-api/algafood-api/src/test/java/com/algafood/CadastroCozinhaApiIT.java
+++ algafood-api/algafood-api/src/test/java/com/algafood/CadastroCozinhaApiIT.java
@@ -2,9 +2,11 @@ package com.algafood;
 
 import static io.restassured.RestAssured.given;
 
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -18,12 +20,17 @@ class CadastroCozinhaApiIT {
 	@LocalServerPort
 	private int port;
 	
+	@Autowired
+	private Flyway flyway;
+	
 	@BeforeEach // vai executar toda vez antes de passar por um teste
 	public void setUp() {
 		//mostra o log quando falha o teste
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
+		
+		flyway.migrate(); //vai dar callback, excluir todos os dados e adicionar os pré-definidos
 	}
 	
 	@Test
@@ -48,6 +55,18 @@ class CadastroCozinhaApiIT {
 		.then()
 		.body("", Matchers.hasSize(4)) // 4cozinhas
 		.body("nome", Matchers.hasItems("Indiana", "Tailandesa")); // cozinhas especifícas
+	}
+	
+	@Test
+	public void deveRetornar201_QuandoCadastrarCozinha() {
+		given()
+			.body("{\"nome\": \"Chinesa\" }")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
 	}
 
 			
