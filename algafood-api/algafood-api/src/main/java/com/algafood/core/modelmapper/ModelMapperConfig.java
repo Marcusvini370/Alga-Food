97df285df4d58342ba02fa948b2914c3f1d86efc
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-01-24 15:33:08 -0300

    Adicionando endereço no modelo da representação do recurso de restaurante

--- algafood-api/algafood-api/src/main/java/com/algafood/core/modelmapper/ModelMapperConfig.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/core/modelmapper/ModelMapperConfig.java
@@ -4,13 +4,31 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.algafood.domain.model.Endereco;
+import com.algafood.domain.model.dto.EnderecoDTO;
+
 
 @Configuration
 public class ModelMapperConfig {
 	
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		
+		var modelMapper = new ModelMapper();
+		
+//		modelMapper.createTypeMap(Restaurante.class, RestauranteDTO.class) //classe que queremos transcrever uma para outra
+//		.addMapping(Restaurante::getTaxaFrete, RestauranteDTO::setPrecoFrete); //adicionar um mapeamento
+		
+		//mapeamento de tipo do endreço pro type model/dto
+		var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoDTO.class);
+		
+		//origem e destino aonde quer atribuir o tipo dele é string, destino e valor = src , src pega a origem do nome do estado
+		enderecoToEnderecoModelTypeMap.<String>addMapping(enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+				(enderecoDtoDest, value) -> enderecoDtoDest.getCidade().setEstado(value));
+		
+	
+	
+			return modelMapper;
 	}
 
 
--- algafood-api/algafood-api/src/main/java/com/algafood/domain/model/dto/RestauranteDTO.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/domain/model/dto/RestauranteDTO.java
@@ -14,6 +14,7 @@ public class RestauranteDTO {
 	private BigDecimal taxaFrete;
 	private CozinhaDTO cozinha;
 	private Boolean ativo;
+	private EnderecoDTO endereco;
 	
 	
 }
