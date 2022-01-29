a1bd6efa3542b06f3f35048ba3c934c91385cf4a
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-01-29 17:00:07 -0300

    Implementando o endpoint de emissão de pedidos

--- algafood-api/algafood-api/src/main/java/com/algafood/api/controller/PedidoController.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/api/controller/PedidoController.java
@@ -2,17 +2,29 @@ package com.algafood.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algafood.api.assembler.PedidoInputDisassembler;
 import com.algafood.api.assembler.PedidoModelAssembler;
 import com.algafood.api.assembler.PedidoResumoModelAssembler;
+import com.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algafood.domain.exception.NegocioException;
 import com.algafood.domain.model.Pedido;
+import com.algafood.domain.model.StatusPedido;
+import com.algafood.domain.model.Usuario;
 import com.algafood.domain.model.dto.PedidoDTO;
 import com.algafood.domain.model.dto.PedidoResumoDTO;
+import com.algafood.domain.model.dto.input.PedidoInput;
 import com.algafood.domain.repository.PedidoRepository;
 import com.algafood.domain.service.EmissaoPedidoService;
 
@@ -32,6 +44,9 @@ public class PedidoController {
     @Autowired
     private PedidoResumoModelAssembler pedidoResumoModelAssembler;
     
+    @Autowired
+    private PedidoInputDisassembler pedidoInputDisassembler;
+    
     @GetMapping
     public List<PedidoResumoDTO> listar() {
         List<Pedido> todosPedidos = pedidoRepository.findAll();
@@ -44,5 +59,25 @@ public class PedidoController {
         Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
         
         return pedidoModelAssembler.toModel(pedido);
-    }            
+    }      
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PedidoDTO adicionar(@Valid @RequestBody PedidoInput pedidoInput) {
+        try {
+            Pedido novoPedido = pedidoInputDisassembler.toDomainObject(pedidoInput);
+
+            // TODO pegar usuário autenticado
+            novoPedido.setCliente(new Usuario());
+            novoPedido.getCliente().setId(1L);
+            novoPedido.setStatus(StatusPedido.CRIADO);
+
+            novoPedido = emissaoPedido.emitir(novoPedido);
+
+            return pedidoModelAssembler.toModel(novoPedido);
+        } catch (EntidadeNaoEncontradaException e) {
+            throw new NegocioException(e.getMessage(), e);
+        }
+    }
+    
 }           
--- algafood-api/algafood-api/src/main/java/com/algafood/core/modelmapper/ModelMapperConfig.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/core/modelmapper/ModelMapperConfig.java
@@ -5,7 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.algafood.domain.model.Endereco;
+import com.algafood.domain.model.ItemPedido;
+import com.algafood.domain.model.Pedido;
 import com.algafood.domain.model.dto.EnderecoDTO;
+import com.algafood.domain.model.dto.PedidoDTO;
+import com.algafood.domain.model.dto.input.ItemPedidoInput;
 
 
 @Configuration
@@ -19,15 +23,15 @@ public class ModelMapperConfig {
 //		modelMapper.createTypeMap(Restaurante.class, RestauranteDTO.class) //classe que queremos transcrever uma para outra
 //		.addMapping(Restaurante::getTaxaFrete, RestauranteDTO::setPrecoFrete); //adicionar um mapeamento
 		
+		modelMapper.createTypeMap(ItemPedidoInput.class, ItemPedido.class)
+	    .addMappings(mapper -> mapper.skip(ItemPedido::setId));  
+			
 		//mapeamento de tipo do endreço pro type model/dto
 		var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoDTO.class);
 		
 		//origem e destino aonde quer atribuir o tipo dele é string, destino e valor = src , src pega a origem do nome do estado
 		enderecoToEnderecoModelTypeMap.<String>addMapping(enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
 				(enderecoDtoDest, value) -> enderecoDtoDest.getCidade().setEstado(value));
-		
-	
-	
 			return modelMapper;
 	}
 
