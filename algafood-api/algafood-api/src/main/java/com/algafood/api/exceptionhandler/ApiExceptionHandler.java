1eceb3d31f54207a7633ad06095c5d8ab792ca89
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-02-01 20:07:30 -0300

    implementando paginação e ordenação de pedidos

--- algafood-api/algafood-api/src/main/java/com/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -40,13 +41,32 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	@Autowired
 	private MessageSource messageSource;
 
-	private ResponseEntity<Object> handleValidationInternal(Exception ex, BindingResult bindingResult,
+	@Override
+	protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status,
+			WebRequest request) {
+
+		return handleValidantionInternal(ex, headers, status, request, ex.getBindingResult());
+	}
+
+	@ExceptionHandler({ ValidacaoException.class })
+	public ResponseEntity<Object> handleValidacaoException(ValidacaoException ex, WebRequest request) {
+		return handleExceptionInternal(ex, ex.getBindingResult(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 
+		return handleValidantionInternal(ex, headers, status, request, ex.getBindingResult());
+	}
+
+	private ResponseEntity<Object> handleValidantionInternal(Exception ex, HttpHeaders headers, HttpStatus status,
+			WebRequest request, BindingResult bindingResult) {
 		ProblemType problemType = ProblemType.DADOS_INVALIDOS;
 		String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
 
 		List<Problem.Object> problemObjects = bindingResult.getAllErrors().stream().map(objectError -> {
+
 			String message = messageSource.getMessage(objectError, LocaleContextHolder.getLocale());
 
 			String name = objectError.getObjectName();
@@ -62,42 +82,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 				.build();
 
 		return handleExceptionInternal(ex, problem, headers, status, request);
-	}
-
-	@ExceptionHandler({ ValidacaoException.class })
-	public ResponseEntity<Object> handleValidacaoException(ValidacaoException ex, WebRequest request) {
-		return handleValidationInternal(ex, ex.getBindingResult(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
-	}
-
-	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-
-		ProblemType problemType = ProblemType.DADOS_INVALIDOS;
-		String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
-
-		BindingResult bildBindingResult = ex.getBindingResult(); // acesso a quais fields ou propriedades foram violadas
-																	// as msg etc
-
-		List<Problem.Object> problemObjects = bildBindingResult.getAllErrors().stream().map(objectError -> {
-
-			String message = messageSource.getMessage(objectError, LocaleContextHolder.getLocale());
-
-			String name = objectError.getObjectName();
-
-			if (objectError instanceof FieldError) {
-				name = ((FieldError) objectError).getField();
-			}
-
-			return Problem.Object.builder().name(name).userMessage(message).build();
-		}).collect(Collectors.toList());
-
-		Problem problem = createProblemBuilder(status, problemType, detail)
-		        .userMessage(detail)
-		        .objects(problemObjects)
-		        .build();
-		    
-		    return handleExceptionInternal(ex, problem, headers, status, request);
 	}
 
 	@ExceptionHandler(Exception.class)
@@ -159,13 +143,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		String path = joinPath(ex.getPath());
 
 		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
-		String detail = String.format("A propriedade '%s' não existe. "
-				+ "Corrija ou remova essa propriedade e tente novamente.", path);
-	
-		Problem problem = createProblemBuilder(status, problemType, detail)
-				.userMessage(MSG_ERRO_GENERICA_USUARIO_FINAL)
+		String detail = String.format(
+				"A propriedade '%s' não existe. " + "Corrija ou remova essa propriedade e tente novamente.", path);
+
+		Problem problem = createProblemBuilder(status, problemType, detail).userMessage(MSG_ERRO_GENERICA_USUARIO_FINAL)
 				.build();
-		
+
 		return handleExceptionInternal(ex, problem, headers, status, request);
 	}
 
@@ -217,13 +200,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		String detail = ex.getMessage();
 
 		Problem problem = createProblemBuilder(status, problemType, detail).userMessage(detail).build();
-
-		// Problem problem = Problem.builder()
-		// .status(status.value())
-		// .type("https://algafood.com.br/entidade-nao-encontrada")
-		// .title("Entidade não encontrada")
-		// .detail(ex.getMessage())
-		// .build();
 
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 
--- algafood-api/algafood-api/src/main/java/com/algafood/infracstruture/repository/spec/PedidoSpecs.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/infracstruture/repository/spec/PedidoSpecs.java
@@ -13,8 +13,10 @@ public class PedidoSpecs {
 
 	public static Specification<Pedido> usandoFiltro(PedidoFilter filtro) {
 		return (root, query, builder) -> {
-			root.fetch("restaurante"); // ajuda com o problema de n + 1
-			root.fetch("cliente");
+			if (Pedido.class.equals(query.getResultType())) {
+				root.fetch("restaurante").fetch("cozinha");
+				root.fetch("cliente");
+			}
 			
 			var predicates = new ArrayList<Predicate>();
 			
