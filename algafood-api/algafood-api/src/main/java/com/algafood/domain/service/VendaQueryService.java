ab9fb6c506c28ae92f7877c03d38970f63842b68
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-02-02 18:20:45 -0300

    Tratando time offset na agregação de vendas diárias por data.

--- algafood-api/algafood-api/src/main/java/com/algafood/domain/service/VendaQueryService.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/domain/service/VendaQueryService.java
@@ -7,7 +7,7 @@ import com.algafood.domain.model.dto.VendaDiaria;
 
 public interface VendaQueryService {
 	
-	List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro);
+	List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro,String timeOffset);
 
 
 }
