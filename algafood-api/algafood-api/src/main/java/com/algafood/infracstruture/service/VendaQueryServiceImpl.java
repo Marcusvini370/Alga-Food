d1dd26dd99416b0decdde6b58e1a1afbb4f3dc6b
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-02-02 17:35:33 -0300

    adicionando os filtros na consulta de vendas diárias

--- algafood-api/algafood-api/src/main/java/com/algafood/infracstruture/service/VendaQueryServiceImpl.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/infracstruture/service/VendaQueryServiceImpl.java
@@ -1,15 +1,18 @@
 package com.algafood.infracstruture.service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.stereotype.Repository;
 
 import com.algafood.domain.filter.VendaDiariaFilter;
 import com.algafood.domain.model.Pedido;
+import com.algafood.domain.model.StatusPedido;
 import com.algafood.domain.model.dto.VendaDiaria;
 import com.algafood.domain.service.VendaQueryService;
 
@@ -24,6 +27,7 @@ public class VendaQueryServiceImpl implements VendaQueryService{
 		var builder = manager.getCriteriaBuilder();
 		var query = builder.createQuery(VendaDiaria.class);
 		var root = query.from(Pedido.class);
+		var predicates = new ArrayList<Predicate>();
 		
 		var functionDateDataCriacao = builder.function(
 				"date", Date.class, root.get("dataCriacao"));
@@ -35,6 +39,24 @@ public class VendaQueryServiceImpl implements VendaQueryService{
 		
 		query.select(selection);
 		query.groupBy(functionDateDataCriacao);
+		
+		if (filtro.getRestauranteId() != null) {
+		    predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+		}
+		    
+		if (filtro.getDataCriacaoInicio() != null) {
+		    predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), 
+		            filtro.getDataCriacaoInicio()));
+		}
+
+		if (filtro.getDataCriacaoFim() != null) {
+		    predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), 
+		            filtro.getDataCriacaoFim()));
+		}
+		    
+		predicates.add(root.get("status").in(
+		        StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+		query.where(predicates.toArray(new Predicate[0]));
 		
 		return manager.createQuery(query).getResultList();
 	}
