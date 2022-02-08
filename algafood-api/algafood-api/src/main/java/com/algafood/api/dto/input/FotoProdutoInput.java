10b38adf671674c3c56debe652555112400fcbce
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-02-08 11:56:06 -0300

    Validando o tamanho máximo do arquivo

--- algafood-api/algafood-api/src/main/java/com/algafood/api/dto/input/FotoProdutoInput.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/api/dto/input/FotoProdutoInput.java
@@ -1,6 +1,11 @@
 package com.algafood.api.dto.input;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.web.multipart.MultipartFile;
+
+import com.algafood.core.validation.FileSize;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +14,11 @@ import lombok.Setter;
 @Setter
 public class FotoProdutoInput {
 	
+	@NotNull
+	@FileSize(max = "13MB")
 	private MultipartFile arquivo;
+	
+	@NotBlank
 	private String descricao;
 
 }
