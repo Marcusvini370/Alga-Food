38733f9fe04dfcf8c9d47b065747b32455a75f93
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-02-08 19:54:12 -0300

    recuperação de foto no serviço de armazenagem

--- algafood-api/algafood-api/src/main/java/com/algafood/infracstruture/service/storage/LocalFotoStorageService.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/infracstruture/service/storage/LocalFotoStorageService.java
@@ -1,6 +1,7 @@
 package com.algafood.infracstruture.service.storage;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -11,23 +12,22 @@ import org.springframework.util.FileCopyUtils;
 import com.algafood.domain.service.FotoStorageService;
 
 @Service
-public class LocalFotoStorageService implements FotoStorageService{
-	
+public class LocalFotoStorageService implements FotoStorageService {
+
 	@Value("${algafood.storage.local.deretorio-fotos}")
 	private Path diretorioFotos;
 
 	@Override
 	public void armazenar(NovaFoto novaFoto) {
-		
+
 		try {
 			Path arquivoPath = getArquivoPath(novaFoto.getNomeArquivo());
-			
-			FileCopyUtils.copy(novaFoto.getInputStream(), 
-					Files.newOutputStream(arquivoPath));
+
+			FileCopyUtils.copy(novaFoto.getInputStream(), Files.newOutputStream(arquivoPath));
 		} catch (IOException e) {
 			throw new StorageException("Não foi possível armazenar o arquivo.", e);
 		}
-		
+
 	}
 
 	private Path getArquivoPath(String nomeArquivo) {
@@ -36,14 +36,25 @@ public class LocalFotoStorageService implements FotoStorageService{
 
 	@Override
 	public void remover(String nomeArquivo) {
-		
+
 		try {
 			Path arquivoPath = getArquivoPath(nomeArquivo);
 			Files.deleteIfExists(arquivoPath);
 		} catch (IOException e) {
 			throw new StorageException("Não foi possível excluir arquivo.", e);
 		}
-		
+
 	}
-	
+
+	@Override
+	public InputStream recuperar(String nomeArquivo) {
+		try {
+			Path arquivoPath = getArquivoPath(nomeArquivo);
+
+			return Files.newInputStream(arquivoPath);
+		} catch (Exception e) {
+			throw new StorageException("Não foi possível recuperar arquivo.", e);
+		}
+	}
+
 }
