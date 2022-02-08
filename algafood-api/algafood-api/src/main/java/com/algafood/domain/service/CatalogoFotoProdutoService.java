8aa1a309c51181f05f0e9c886fd87b5aa22dde09
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-02-08 19:59:53 -0300

    Implementando endpoint de consulta de foto de produto

--- algafood-api/algafood-api/src/main/java/com/algafood/domain/service/CatalogoFotoProdutoService.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.algafood.domain.exception.FotoProdutoNaoEncontradaException;
 import com.algafood.domain.model.FotoProduto;
 import com.algafood.domain.repository.ProdutoRepository;
 import com.algafood.domain.service.FotoStorageService.NovaFoto;
@@ -49,5 +50,10 @@ public class CatalogoFotoProdutoService {
 		
 		return foto;
 	}
+	
+	public FotoProduto buscarOuFalhar(Long restauranteId, Long produtoId) {
+	    return produtoRepository.findFotoById(restauranteId, produtoId)
+	            .orElseThrow(() -> new FotoProdutoNaoEncontradaException(restauranteId, produtoId));
+	}  
 	
 }
