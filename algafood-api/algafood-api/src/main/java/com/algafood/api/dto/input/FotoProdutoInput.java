05d189526925ef28988c9eabd243dcc970a40b82
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-02-08 12:45:57 -0300

    Validando o content type do arquivo

--- algafood-api/algafood-api/src/main/java/com/algafood/api/dto/input/FotoProdutoInput.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/api/dto/input/FotoProdutoInput.java
@@ -3,8 +3,10 @@ package com.algafood.api.dto.input;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.algafood.core.validation.FileContentType;
 import com.algafood.core.validation.FileSize;
 
 import lombok.Getter;
@@ -16,6 +18,7 @@ public class FotoProdutoInput {
 	
 	@NotNull
 	@FileSize(max = "13MB")
+	@FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
 	private MultipartFile arquivo;
 	
 	@NotBlank
