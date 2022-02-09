a2e0aa3025c2d3f0d329c72a7b1a27172f20eb8e
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-02-09 12:47:14 -0300

    Criando bean de propriedades de configuração dos serviços de storage

--- algafood-api/algafood-api/src/main/java/com/algafood/infracstruture/service/storage/LocalFotoStorageService.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/infracstruture/service/storage/LocalFotoStorageService.java
@@ -5,17 +5,18 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
+import com.algafood.core.storage.StorageProperties;
 import com.algafood.domain.service.FotoStorageService;
 
 @Service
 public class LocalFotoStorageService implements FotoStorageService {
 
-	@Value("${algafood.storage.local.diretorio-fotos}")
-	private Path diretorioFotos;
+	@Autowired
+	private StorageProperties storageProperties;
 
 	@Override
 	public void armazenar(NovaFoto novaFoto) {
@@ -31,7 +32,7 @@ public class LocalFotoStorageService implements FotoStorageService {
 	}
 
 	private Path getArquivoPath(String nomeArquivo) {
-		return diretorioFotos.resolve(Path.of(nomeArquivo));
+		return storageProperties.getLocal().getDiretorioFotos().resolve(Path.of(nomeArquivo));
 	}
 
 	@Override
