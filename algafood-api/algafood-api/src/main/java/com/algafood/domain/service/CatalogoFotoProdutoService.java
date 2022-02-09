0fa9522133baa87e4e9b374cc9b9037edc7966bb
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-02-09 15:27:03 -0300

    Implementando a inclusão de objetos no bucket da Amazon S3

--- algafood-api/algafood-api/src/main/java/com/algafood/domain/service/CatalogoFotoProdutoService.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -42,6 +42,7 @@ public class CatalogoFotoProdutoService {
 		
 		NovaFoto novaFoto = NovaFoto.builder()
 				.nomeArquivo(foto.getNomeArquivo())
+				.contentType(foto.getContentType())
 				.inputStream(dadosArquivo)
 				.build();
 		
