f6989a929b8c94ade4b0c8ffb15326596b251a90
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-02-13 15:52:02 -0300

    Implementando requisições condicionais com Deep ETags

--- algafood-api/algafood-api/src/main/java/com/algafood/api/controller/FormaPagamentoController.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/api/controller/FormaPagamentoController.java
@@ -1,5 +1,6 @@
 package com.algafood.api.controller;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -18,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 import com.algafood.api.assembler.FormaPagamentoInputDisassembler;
 import com.algafood.api.assembler.FormaPagamentoModelAssembler;
@@ -44,7 +47,16 @@ public class FormaPagamentoController {
     private FormaPagamentoInputDisassembler formaPagamentoInputDisassembler;
     
     @GetMapping
-	public ResponseEntity<List<FormaPagamentoDTO>> listar() {
+	public ResponseEntity<List<FormaPagamentoDTO>> listar(ServletWebRequest request) {
+    	ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+    	
+    	String eTag = "0";
+    	OffsetDateTime dataUltimaAtualizacao = formaPagamentoRepository.getDataUltimaAtualizacao();
+    	
+    	if(dataUltimaAtualizacao != null) {
+    		eTag = String.valueOf(dataUltimaAtualizacao.toEpochSecond());
+    	}
+    	
 		List<FormaPagamento> todasFormasPagamentos = formaPagamentoRepository.findAll();
 		
 		List<FormaPagamentoDTO> formasPagamentosModel = formaPagamentoModelAssembler
@@ -52,6 +64,7 @@ public class FormaPagamentoController {
 		
 		return ResponseEntity.ok()
 				.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+				.eTag(eTag)
 				//.cachePrivate é cache armazenado em caches locais
 				//.cachePulbic é armazenado em caches locais e compartilhados
 				//.cacheControl(CacheControl.noCache()) //ao fazer cache vai precisar fazer validação como na etag
