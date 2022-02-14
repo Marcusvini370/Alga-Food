c13114da3f02e765d00c916f451200378d7847b5
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-02-14 13:14:22 -0300

    Descrevendo códigos de status de respostas de forma global

--- algafood-api/algafood-api/src/main/java/com/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/core/openapi/SpringFoxConfig.java
@@ -1,13 +1,20 @@
 package com.algafood.core.openapi;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -24,9 +31,27 @@ public class SpringFoxConfig {
           .paths(PathSelectors.any())
          // .paths(PathSelectors.ant("/restaurantes/*")) controlador específico
           .build() //retorna o docket;
+          .useDefaultResponseMessages(false) //desativa os código de erro gerado auto
+          .globalResponses(HttpMethod.GET, globalGetResponseMessages()) //conf cod de status padrao pro mét get
           .apiInfo(apiInfo()) //traz as configurações do método para a documentação
           .tags(new Tag("Cidades", "Gerencia as cidades"));
   }
+  
+  
+
+		private List<Response> globalGetResponseMessages() {
+		  return Arrays.asList(
+		      new ResponseBuilder()
+		          .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+		          .description("Erro interno do Servidor")
+		          .build(),
+		      new ResponseBuilder()
+		          .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value())) // exemplo tentar acessar por xml, sendo que é json
+		          .description("Recurso não possui representação que pode ser aceita pelo consumidor")
+		          .build()
+		  );
+		}
+  
   
   
   public ApiInfo apiInfo() {
