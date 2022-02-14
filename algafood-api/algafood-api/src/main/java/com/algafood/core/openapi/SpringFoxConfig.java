f34bcc54c9a5f1e86eef2b679a23e68ca0b1009c
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-02-14 14:11:12 -0300

    Referenciando modelo de representação de problema com códigos de status de erro

--- algafood-api/algafood-api/src/main/java/com/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/core/openapi/SpringFoxConfig.java
@@ -2,11 +2,13 @@ package com.algafood.core.openapi;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import com.algafood.api.exceptionhandler.Problem;
 import com.fasterxml.classmate.TypeResolver;
@@ -14,6 +16,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RepresentationBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
@@ -49,52 +52,75 @@ public class SpringFoxConfig {
           .tags(new Tag("Cidades", "Gerencia as cidades"));
   }
   
-  private List<Response> globalPostPutResponseMessages() {
+  /* O método responseModel não existe na classe ResponseBuilder do SpringFox 3,
+   *  assim como o ModelRef que foi depreciado. Vamos utilizar os métodos
+   *   representation e apply para realizar a mesma configuração. */
+  
+  //método que ira gerar a referência para classe Problem:
+  private Consumer<RepresentationBuilder> getProblemaModelReference() {
+	    return r -> r.model(m -> m.name("Problema")
+	            .referenceModel(ref -> ref.key(k -> k.qualifiedModelName(
+	                    q -> q.name("Problema").namespace("com.algafood.api.exceptionhandler")))));
+	}
+  
+  private List<Response> globalGetResponseMessages() {
 	    return Arrays.asList(
-	        new ResponseBuilder()
-	            .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
-	            .description("Requisição inválida (erro do cliente)")
-	            .build(),
-	        new ResponseBuilder()
-	            .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-	            .description("Erro interno no servidor")
-	            .build(),
-	        new ResponseBuilder()
-	            .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
-	            .description("Recurso não possui representação que poderia ser aceita pelo consumidor")
-	            .build(),
-	        new ResponseBuilder()
-	            .code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
-	            .description("Requisição recusada porque o corpo está em um formato não suportado")
-	            .build()
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+	                    .description("Erro interno do Servidor")
+	                    .representation( MediaType.APPLICATION_JSON )
+	                    .apply(getProblemaModelReference())
+	                    .build(),
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+	                    .description("Recurso não possui representação que pode ser aceita pelo consumidor")
+	                    .build()
 	    );
-	  }
+	}
 
-	  private List<Response> globalDeleteResponseMessages() {
+	private List<Response> globalPostPutResponseMessages() {
 	    return Arrays.asList(
-	        new ResponseBuilder()
-	            .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
-	            .description("Requisição inválida (erro do cliente)")
-	            .build(),
-	        new ResponseBuilder()
-	            .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-	            .description("Erro interno no servidor")
-	            .build()
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+	                    .description("Requisição inválida (erro do cliente)")
+	                    .representation( MediaType.APPLICATION_JSON )
+	                    .apply(getProblemaModelReference())
+	                    .build(),
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+	                    .description("Erro interno no servidor")
+	                    .representation( MediaType.APPLICATION_JSON )
+	                    .apply(getProblemaModelReference())
+	                    .build(),
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+	                    .description("Recurso não possui representação que poderia ser aceita pelo consumidor")
+	                    .build(),
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
+	                    .description("Requisição recusada porque o corpo está em um formato não suportado")
+	                    .representation( MediaType.APPLICATION_JSON )
+	                    .apply(getProblemaModelReference())
+	                    .build()
 	    );
-	  }
+	}
 
-		private List<Response> globalGetResponseMessages() {
-		  return Arrays.asList(
-		      new ResponseBuilder()
-		          .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-		          .description("Erro interno do Servidor")
-		          .build(),
-		      new ResponseBuilder()
-		          .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value())) // exemplo tentar acessar por xml, sendo que é json
-		          .description("Recurso não possui representação que pode ser aceita pelo consumidor")
-		          .build()
-		  );
-		}
+	private List<Response> globalDeleteResponseMessages() {
+	    return Arrays.asList(
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+	                    .description("Requisição inválida (erro do cliente)")
+	                    .representation( MediaType.APPLICATION_JSON )
+	                    .apply(getProblemaModelReference())
+	                    .build(),
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+	                    .description("Erro interno no servidor")
+	                    .representation( MediaType.APPLICATION_JSON )
+	                    .apply(getProblemaModelReference())
+	                    .build()
+	    );
+	}
   
 		@Bean
 		public JacksonModuleRegistrar springFoxJacksonConfig() {
