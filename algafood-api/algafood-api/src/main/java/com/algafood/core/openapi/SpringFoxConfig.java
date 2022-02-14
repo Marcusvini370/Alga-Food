4fa9adff724d1ec2b2b8bdd947422a9c659b294a
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-02-14 10:32:26 -0300

    Gerando a definição OpenAPI em JSON com SpringFox

---
+++ algafood-api/algafood-api/src/main/java/com/algafood/core/openapi/SpringFoxConfig.java
@@ -0,0 +1,20 @@
+package com.algafood.core.openapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+
+  @Bean
+  public Docket apiDocket() {
+    return new Docket(DocumentationType.OAS_30) //configuração da documentação
+        .select() //retornando builder para selecionar os endpoints para sempre expostos
+          .apis(RequestHandlerSelectors.any()) // seleciona tudo em relação a api para documentar
+          .build(); //retorna o docket;
+  }
+  
+}
