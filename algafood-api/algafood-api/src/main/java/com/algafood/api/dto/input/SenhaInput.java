fa818059b6169d3449f52ee911993fea5a79f044
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-02-15 18:42:06 -0300

    descrevendo documentação de endpoints de usuários

--- algafood-api/algafood-api/src/main/java/com/algafood/api/dto/input/SenhaInput.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/api/dto/input/SenhaInput.java
@@ -4,6 +4,7 @@ import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,9 +13,11 @@ import lombok.Setter;
 @Getter
 public class SenhaInput extends UsuarioInput {
 
+	@ApiModelProperty(example = "123", required = true)
 	@NotBlank
 	private String senhaAtual;
 
+	@ApiModelProperty(example = "123", required = true)
 	@NotBlank
 	private String novaSenha;
 }
--- algafood-api/algafood-api/src/main/java/com/algafood/api/dto/input/UsuarioInput.java
+++ algafood-api/algafood-api/src/main/java/com/algafood/api/dto/input/UsuarioInput.java
@@ -3,6 +3,7 @@ package com.algafood.api.dto.input;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,10 +11,12 @@ import lombok.Setter;
 @Getter
 public class UsuarioInput {
 
-    @NotBlank
-    private String nome;
-    
-    @NotBlank
-    @Email
-    private String email;
+	@ApiModelProperty(example = "João da Silva", required = true)
+	@NotBlank
+	private String nome;
+
+	@ApiModelProperty(example = "joao.ger@algafood.com.br", required = true)
+	@NotBlank
+	@Email
+	private String email; 
 }           
