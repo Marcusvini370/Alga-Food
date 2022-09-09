69ff69685cbed3cb89952e6b7b9d500fd9c14322
Marcus Vinicius <marcusvini370@gmail.com>
2022-09-09 14:30:45 -0300

    Refatorando controladores para adicionar /v1 nas URIs

--- algafood-api/src/main/java/com/algafood/api/v1/controller/RestauranteUsuarioResponsavelController.java
+++ algafood-api/src/main/java/com/algafood/api/v1/controller/RestauranteUsuarioResponsavelController.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/restaurantes/{restauranteId}/responsaveis",
+@RequestMapping(path = "/v1/restaurantes/{restauranteId}/responsaveis",
         produces = MediaType.APPLICATION_JSON_VALUE)
 public class RestauranteUsuarioResponsavelController implements RestauranteUsuarioResponsavelControllerOpenApi {
 
--- algafood-api/src/main/java/com/algafood/api/v1/controller/TesteController.java
+++ algafood-api/src/main/java/com/algafood/api/v1/controller/TesteController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/teste")
+@RequestMapping("/v1/teste")
 public class TesteController {
 
     @Autowired
