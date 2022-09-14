1b6f83526a1abfa4be8b2fe066933b7e051f3387
Marcus Vinicius <marcusvini370@gmail.com>
2022-09-14 12:37:49 -0300

    Configurando o Resource Server para JWT assinado com chave simétrica

--- algafood-api/src/main/java/com/algafood/core/security/ResourceServeConfig.java
+++ algafood-api/src/main/java/com/algafood/core/security/ResourceServeConfig.java
@@ -1,9 +1,15 @@
 package com.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration // classe de configuração do spring
 @EnableWebSecurity
@@ -15,7 +21,17 @@ public class ResourceServeConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests() //inicia configurações de autorização de acesso as requisições
                     .anyRequest().authenticated() //autoriza todas requisições autenticada
                 .and()
+                .cors().and()
                 .oauth2ResourceServer() //habilitando resource server na api
-                    .opaqueToken();
+                    .jwt();
     }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var secretKey = new SecretKeySpec("d9721qghe982gqhr98qwhaouidhas89dh80hj389024y890sahd98iuawsgh81q2".getBytes(),
+                "HmacSHA256");
+
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
+    }
+
 }
