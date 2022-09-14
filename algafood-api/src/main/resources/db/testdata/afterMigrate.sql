93278a504b10bcd33d4897e70412953b0f550a88
Marcus Vinicius <marcusvini370@gmail.com>
2022-09-14 14:58:44 -0300

    Adicionando Bcrypt nas senhas dos usuáros no migrate

--- algafood-api/src/main/resources/db/testdata/afterMigrate.sql
+++ algafood-api/src/main/resources/db/testdata/afterMigrate.sql
@@ -130,12 +130,12 @@ insert into grupo_permissao(grupo_id, permissao_id)
 values (1, 1), (1, 2), (2, 1), (2, 2), (3, 1);
 
 insert into usuario(id, nome, email, senha, data_cadastro)
-values (1, 'João da Silva', 'joao.ger@algafood.com', '123', utc_timestamp),
-(2, 'Maria Joaquina', 'maria.vnd@algafood.com', '123', utc_timestamp),
-(3, 'José Souza', 'jose.aux@algafood.com', '123', utc_timestamp),
-(4, 'Sebastião Martins', 'sebastiao.cad@algafood.com', '123', utc_timestamp),
-(5, 'Marcus Vinicius', 'marcusvini370@gmail.com', '123', utc_timestamp),
-(6, 'Marcus Vinicius 2', 'marcusvini370+teste@gmail.com', '123', utc_timestamp);
+values (1, 'João da Silva', 'joao.ger@algafood.com', '$2a$12$pY8XKi.kW8lnpaQtm9xA9upitDA0eDl5ON0V7vBWGN4rnWeW3ZomO', utc_timestamp),
+(2, 'Maria Joaquina', 'maria.vnd@algafood.com', '$2a$12$pY8XKi.kW8lnpaQtm9xA9upitDA0eDl5ON0V7vBWGN4rnWeW3ZomO', utc_timestamp),
+(3, 'José Souza', 'jose.aux@algafood.com', '$2a$12$pY8XKi.kW8lnpaQtm9xA9upitDA0eDl5ON0V7vBWGN4rnWeW3ZomO', utc_timestamp),
+(4, 'Sebastião Martins', 'sebastiao.cad@algafood.com', '$2a$12$pY8XKi.kW8lnpaQtm9xA9upitDA0eDl5ON0V7vBWGN4rnWeW3ZomO', utc_timestamp),
+(5, 'Marcus Vinicius', 'marcusvini370@gmail.com', '$2a$12$pY8XKi.kW8lnpaQtm9xA9upitDA0eDl5ON0V7vBWGN4rnWeW3ZomO', utc_timestamp),
+(6, 'Marcus Vinicius 2', 'marcusvini370+teste@gmail.com', '$2a$12$pY8XKi.kW8lnpaQtm9xA9upitDA0eDl5ON0V7vBWGN4rnWeW3ZomO', utc_timestamp);
 
 insert into usuario_grupo(usuario_id, grupo_id)
 values (1, 1), (1, 2), (2, 2);
