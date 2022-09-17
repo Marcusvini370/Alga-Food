0f6d42341f9595992efaedb762ba3bcb9e678c8e
Marcus Vinicius <marcusvini370@gmail.com>
2022-09-17 11:37:51 -0300

    restringindo acessos aos endpoints de transição de status de pedidos

--- algafood-api/src/main/java/com/algafood/api/v1/controller/FluxoPedidoController.java
+++ algafood-api/src/main/java/com/algafood/api/v1/controller/FluxoPedidoController.java
@@ -1,6 +1,7 @@
 package com.algafood.api.v1.controller;
 
 import com.algafood.api.v1.openapi.controller.FluxoPedidoControllerOpenApi;
+import com.algafood.core.security.CheckSecurity;
 import com.algafood.domain.service.FluxoPedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,7 @@ public class FluxoPedidoController implements FluxoPedidoControllerOpenApi {
     @Autowired
     private FluxoPedidoService fluxoPedido;
 
+    @CheckSecurity.Pedidos.PodeGerenciarPedidos
     @PutMapping("/confirmacao")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> confirmar(@PathVariable String codigoPedido) {
@@ -23,6 +25,7 @@ public class FluxoPedidoController implements FluxoPedidoControllerOpenApi {
         return ResponseEntity.noContent().build();
     }
 
+    @CheckSecurity.Pedidos.PodeGerenciarPedidos
     @PutMapping("/cancelamento")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> cancelar(@PathVariable String codigoPedido) {
@@ -30,6 +33,7 @@ public class FluxoPedidoController implements FluxoPedidoControllerOpenApi {
         return ResponseEntity.noContent().build();
     }
 
+    @CheckSecurity.Pedidos.PodeGerenciarPedidos
     @PutMapping("/entrega")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> entregar(@PathVariable String codigoPedido) {
--- algafood-api/src/main/java/com/algafood/core/security/AlgaSecurity.java
+++ algafood-api/src/main/java/com/algafood/core/security/AlgaSecurity.java
@@ -1,5 +1,6 @@
 package com.algafood.core.security;
 
+import com.algafood.domain.repository.PedidoRepository;
 import com.algafood.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -12,6 +13,9 @@ public class AlgaSecurity {
 
 	@Autowired
 	private RestauranteRepository restauranteRepository;
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
 
 	public Authentication getAuthentication() {
 		return SecurityContextHolder.getContext().getAuthentication();
@@ -28,6 +32,10 @@ public class AlgaSecurity {
 			return false;
 		}
 		return restauranteRepository.existsResponsavel(restauranteId, getUsuarioId());
+	}
+
+	public boolean gerenciaRestauranteDoPedido(String codigoPedido) {
+		return pedidoRepository.isPedidoGerenciadoPor(codigoPedido, getUsuarioId());
 	}
 	
 }
